e1e8d6cd631b001481a2e2465d813e090b88c09d
Froylan Aguilar Contreras <fac23914@gmail.com>
2022-12-29 13:00:34 -0600

    se agrego un espacio en el html y se modifico el metodo codificar para que el texto de salida lo coloque en el area de salida

--- cod-decod.js
+++ cod-decod.js
@@ -1,5 +1,8 @@
+let txtEntrada = document.getElementById("entrada");
+let areaSalida = document.getElementById("salida");
 function encriptar(){
-	alert("Encriptar");
+	let texto = txtEntrada.value;	
+	areaSalida.innerHTML=texto;
 }
 
 function desencriptar(){
