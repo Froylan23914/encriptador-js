f0c7a3a309e5e8962427f17220398fe6d89e7cf5
Froylan Aguilar Contreras <fac23914@gmail.com>
2022-12-29 14:11:29 -0600

    se modifico el metodo codificar y limpia texto de entrada

--- cod-decod.js
+++ cod-decod.js
@@ -1,8 +1,39 @@
 let txtEntrada = document.getElementById("entrada");
 let areaSalida = document.getElementById("salida");
+const llaves = {
+	a:	"ai",
+	e:	"enter",
+	i:	"imes",
+	o:	"ober",
+	u:	"ufat"
+}
+
 function encriptar(){
 	let texto = txtEntrada.value;	
-	areaSalida.innerHTML=texto;
+	txtEntrada.value="";
+	let textoSalida="";
+	for(let i=0;i<texto.length;i++){
+		switch(texto.charAt(i)){
+			case "a":
+			textoSalida+=llaves.a;
+			break;
+			case "e":
+			textoSalida+=llaves.e;
+			break;
+			case "i":
+			textoSalida+=llaves.i;
+			break;
+			case "o":
+			textoSalida+=llaves.o;
+			break;
+			case "u":
+			textoSalida+=llaves.u;
+			break;
+			default:
+			textoSalida+=texto.charAt(i);
+		}
+	}
+	areaSalida.value=textoSalida;
 }
 
 function desencriptar(){
